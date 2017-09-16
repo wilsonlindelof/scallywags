a56f0aaa42495e534920108ff3a895e71648710e
Wilson Lindelof <wilson@accelerantapps.com>
2017-09-16 12:43:45 -0600

    Random Game Code, Joining Game, Lobby, Selecting a Team

--- commandclient/commandclient.js
+++ commandclient/commandclient.js
@@ -1,18 +1,14 @@
 
 var sessionId = null;
-var room = 'ZJPRO69420';
+var nickname = '';
+var UID = '';
+var team = 'grey';
+var role = 'spectator';
 
-var helloButton = document.getElementById('helloButton');
 var helloButton2 = document.getElementById('helloButton2');
-
-helloButton.addEventListener('click', function() {
-	console.log('HELLO BUTTON');
-	var state = {
-		'x': '50',
-		'y': 0
-	};
-	webrtc.sendToAll('someType', state);
-}, false);
+var joinGameButton = document.getElementById('joinGameButton');
+var redTeamButton = document.getElementById('redTeamButton');
+var blueTeamButton = document.getElementById('blueTeamButton');
 
 helloButton2.addEventListener('click', function() {
 	console.log('HELLO BUTTON2');
@@ -23,27 +19,87 @@ helloButton2.addEventListener('click', function() {
 	webrtc.sendDirectlyToAll('someLabel', 'someType', state2);
 }, false);
 
+joinGameButton.addEventListener('click', function() {
+	console.log('Join Game');
+	var gameCodeInput = document.getElementById('gameCode');	
+	var gameCode = gameCodeInput.value;
+	var nicknameInput = document.getElementById('nickname');
+	nickname = nicknameInput.value;
+	if (!nickname || !(nickname.length > 0)) {
+		console.log('ENTER YOUR NICKNAME');
+	} else {
+		console.log(gameCode);
+		webrtc.joinRoom(gameCode, function (err, res) {
+			console.log('joined', gameCode, err, res);
+			UID = generateUID(8, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+			var player = {
+				'nickname': nickname,
+				'UID': UID,
+				'team': team,
+				'role': role
+			};
+			document.getElementById('initialInput').style.display = 'none';
+			document.getElementById('teamAndRoleInput').style.display = 'block';
+			setTimeout(function() {
+				console.log('just gonna send it');
+				webrtc.sendDirectlyToAll('GAME', 'PLAYER_JOINED', player);
+			}, 1000);			
+		});
+	}
+}, false);
+
+redTeamButton.addEventListener('click', function() {
+	console.log('Join Red Team');
+	team = 'red';
+	var player = {
+		'nickname': nickname,
+		'UID': UID,
+		'team': team,
+		'role': role
+	};
+	webrtc.sendDirectlyToAll('GAME', 'PLAYER_SELECTED_TEAM', player);
+}, false);
+
+blueTeamButton.addEventListener('click', function() {
+	console.log('Join Blue Team');
+	team = 'blue';
+	var player = {
+		'nickname': nickname,
+		'UID': UID,
+		'team': team,
+		'role': role
+	};
+	webrtc.sendDirectlyToAll('GAME', 'PLAYER_SELECTED_TEAM', player);
+}, false);
+
+function generateUID(length, chars) {
+	console.log("GENERATE UID");
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
 var webrtc = new SimpleWebRTC({
     autoRequestMedia: false,
-	debug: true,
+	debug: false,
 	enableDataChannels: true,
 });
 
 // we have to wait until it's ready
 webrtc.on('readyToCall', function () {		
     // you can name it anything
-	console.log('READY TO CALL');
-    webrtc.joinRoom(room, function (err, res) {
+	console.log('READY TO CALL THIS HSOULDNT HAPPEN');
+    /*webrtc.joinRoom(room, function (err, res) {
         console.log('joined', room, err, res);
-    });
+    });*/
 });
 
 initialize = function() {
 	console.log('INITIALIZE');
 	
-	webrtc.joinRoom(room, function (err, res) {
+	/*webrtc.joinRoom(room, function (err, res) {
 		console.log('joined', room, err, res);
-	});
+	});*/
 }
 
 // time to log when thing has started
--- gameclient/gameclient.js
+++ gameclient/gameclient.js
@@ -1,6 +1,6 @@
 
 var sessionId = null;
-var room = 'ZJPRO69420';
+var players = [];
 
 var webrtc = new SimpleWebRTC({
     autoRequestMedia: false,
@@ -11,17 +11,27 @@ var webrtc = new SimpleWebRTC({
 // we have to wait until it's ready
 webrtc.on('readyToCall', function () {		
     // you can name it anything
-	console.log('READY TO CALL');
-	webrtc.createRoom(room, function (err, res) {
+	console.log('READY TO CALL SHOULDNT HAPPEN');
+	/*webrtc.createRoom(room, function (err, res) {
         console.log('joined', room, err, res);
-    });
+    });*/
 });
 
 initialize = function() {
 	console.log('INITIALIZE');
-	webrtc.createRoom(room, function (err, res) {
-		console.log('joined', room, err, res);
+	var gameCode = generateGameCode(6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+	var gameCodeSpan = document.getElementById('gameCode');
+    gameCodeSpan.innerHTML = gameCode;
+	webrtc.createRoom(gameCode, function (err, res) {
+		console.log('joined', gameCode, err, res);
 	});
+}
+
+function generateGameCode(length, chars) {
+	console.log("GENERATE GAME CODE");
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
 }
 
 // time to log when thing has started
@@ -40,18 +50,75 @@ webrtc.on('leftRoom', function (roomName) {
     console.log('LEFT ROOM BYE BYE: ', roomName);
 });
 
-getPeers = function() {		
-	console.log('get peers!');
-	setTimeout(function() {
-		if (sessionId) {
-			var peers = webrtc.getPeers(sessionId);
-			console.log('PEERS ', peers);
-		} else {
-			console.log('Didnt get peers because sessionId is ', sessionId);
+webrtc.on('channelMessage', function (peer, label, data) {
+	// Only handle messages from your dataChannel
+	console.log("ON MESSAGE", peer, label, data);
+	console.log("PEER", peer);
+	console.log("LABEL", label);
+	console.log("DATA", data);
+	/*if (label !== 'text chat') return;
+	else if (data.type == 'chat') {
+	console.log('Received message: ' + data.payload + ' from ' + peer.id);
+	}*/
+	//game client should only listen to GAME label messages
+	if (label === 'GAME') {
+		switch (data.type) {
+			case 'PLAYER_JOINED':
+				console.log('A new Player has joined!');
+				var player = {
+					'nickname': data.payload.nickname,
+					'UID': data.payload.UID,
+					'team': data.payload.team,
+					'role': data.payload.role
+				};
+				players.push(player);
+				updatePlayerLobby();
+				break;
+			case 'PLAYER_SELECTED_TEAM':
+				console.log('A player has selected a team');
+				for (var i = 0; i < players.length; i++) {
+					if (players[i].UID === data.payload.UID) {
+						console.log(players[i], 'MATCH');
+						console.log('going to set players team from ', players[i].team, ' to ', data.payload.team);
+						console.log(players[i].team);
+						players[i].team = data.payload.team;
+						console.log(players[i].team);
+					}
+				}
+				updatePlayerLobby();
+				break;
+			default:
+				console.log('WARNING, ERROR! TYPE DIDNT MATCH!');
 		}
-		getPeers();
-	}, 5000);
+	}
+});
+
+function updatePlayerLobby() {
+	console.log('UPDATE PLAYER LOBBY');
+	console.log(players);
+	var redTeamPlayersDiv = document.getElementById('redTeamPlayers');
+	var greyTeamPlayersDiv = document.getElementById('greyTeamPlayers');
+	var blueTeamPlayersDiv = document.getElementById('blueTeamPlayers');
+	
+	var redTeamPlayersHTML = '<ul>';
+	var greyTeamPlayersHTML = '<ul>';
+	var blueTeamPlayersHTML = '<ul>';
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].team === 'red') {
+			redTeamPlayersHTML += '<li>' + players[i].nickname + ' - ' + players[i].role + '</li>';
+		} else if (players[i].team === 'grey') {
+			greyTeamPlayersHTML += '<li>' + players[i].nickname + ' - ' + players[i].role + '</li>';
+		} else if (players[i].team === 'blue') {
+			blueTeamPlayersHTML += '<li>' + players[i].nickname + ' - ' + players[i].role + '</li>';
+		}					
+	}
+	redTeamPlayersHTML += '</ul>';
+	greyTeamPlayersHTML += '</ul>';
+	blueTeamPlayersHTML += '</ul>';
+	
+	redTeamPlayersDiv.innerHTML = redTeamPlayersHTML;
+	greyTeamPlayersDiv.innerHTML = greyTeamPlayersHTML;
+	blueTeamPlayersDiv.innerHTML = blueTeamPlayersHTML;
 }
 
-getPeers();
 initialize();
